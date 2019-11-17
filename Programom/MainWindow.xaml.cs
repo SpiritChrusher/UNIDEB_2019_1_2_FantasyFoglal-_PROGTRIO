0399f678a1c4cdb67d23650fe679c5384c6bce33
Kristóf Szabó <szabokristof997@gmail.com>
2019-11-17 23:12:01 +0100

    Map update

--- Programom/MainWindow.xaml.cs
+++ Programom/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -56,6 +57,28 @@ namespace Programom
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Hungary_Click(object sender, RoutedEventArgs e)
+        {
+            Popup codePopup = new Popup();
+            TextBlock popupText = new TextBlock();
+            popupText.Text = "Ez a funkció még yet to come";
+            popupText.Background = Brushes.LightBlue;
+            popupText.Foreground = Brushes.Blue;
+            codePopup.Child = popupText;
+        }
+
+        private void Középfölde_Click(object sender, RoutedEventArgs e)
+        {
+            Middle_Earth középföld = new Middle_Earth();
+            this.Content = középföld; 
+        }
+
+        private void Star_Wars_Click(object sender, RoutedEventArgs e)
+        {
+            Star_Wars tatooine = new Star_Wars();
+            this.Content = tatooine;
         }
     }
 }
